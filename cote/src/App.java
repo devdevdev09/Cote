968cc1a22faffc1f5135383919dc2d0593a3b000
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-08-27 21:50:06 +0900

    cote 1

---
+++ cote/src/App.java
@@ -0,0 +1,9 @@
+import exam.Solution;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution sol = new Solution();
+        String dartResult = "1D2S#10S";
+        sol.solution(dartResult);
+    }
+}
