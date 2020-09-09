7a9b5557cce194fa785936b7a78d46cbdec64d68
dev09 <devdevdevdevdev09@gmail.com>
2020-09-09 15:09:20 +0900

    test cote

--- cote/src/exam/Solution0901_2.java
+++ cote/src/exam/Solution0901_2.java
@@ -5,9 +5,22 @@ import java.util.List;
 
 public class Solution0901_2 {
 
+// 제한사항
+// s의 길이는 1 이상 1,000 이하입니다.
+// s는 알파벳 소문자로만 이루어져 있습니다.
+
+// "aabbaccc"	7
+// "ababcdcdababcdcd"	9
+// "abcabcdede"	8
+// "abcabcabcabcdededededede"	14
+// "xababcdcdababcdcd"	17
+
     public static void main(String[] args) {
         Solution0901_2 solution = new Solution0901_2();
-        solution.solution("abcabcdede");
+
+        String str = "xababcdcdababcdcd";
+        int result = solution.solution(str);
+        System.out.println(str + " : result = " + result);
     }
 
     public int solution(String s){
@@ -25,28 +38,30 @@ public class Solution0901_2 {
         return min;
     }
 
+    // len : 5, i=[0,1,2,3,4] > i=3,
+    // ex a,b,c,d,d => 1a1b1c2d
+    // ex a,b,c,d,d,e => 1a1b1c2d1e
+
     public int getPressCount(String[] arr){
         String temp = "";
         int cnt = 1;
         String result = "";
         
-        for(int i = 1; i < arr.length; i++){
-            temp = arr[i-1];
-            if(temp.equals(arr[i])){
+        for(int i = 0; i < arr.length-1; i++){
+           if(arr[i].equals(arr[i+1])){
                 cnt++;
+           }else{
                 result += Integer.toString(cnt) + arr[i];
-            }else{
-                result += Integer.toString(cnt) + arr[i]; 
                 cnt = 1;
-            }
+           }
 
-            // if(i+1 == arr.length-1){
-            //     if(temp.equals(arr[i+1])){
-            //         result += Integer.toString(cnt) + arr[i]; 
-            //     }else{
-            //        result += Integer.toString(1) + arr[i+1]; 
-            //     }
-            // }
+           if(i+1 == arr.length-1){
+                if(arr[i].equals(arr[i+1])){
+                    result += Integer.toString(cnt+1) + arr[i];
+                }else{
+                    result += Integer.toString(cnt) + arr[i+1];
+                }
+           }
         }
 
         int count = result.replaceAll("1", "").length();
@@ -67,7 +82,9 @@ public class Solution0901_2 {
             }
 
             if(end > s.length()){
-                list.add(s.substring(start));    
+                if(!"".equals(s.substring(start))){
+                    list.add(s.substring(start));    
+                }
                 break;
             }
             
@@ -77,5 +94,4 @@ public class Solution0901_2 {
 
         return list.toArray(new String[list.size()]);
     }
-    
 }
