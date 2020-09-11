269a6dc897201c4017c92b0c6ee35bee5f44050c
dev09 <devdevdevdevdev09@gmail.com>
2020-09-11 10:53:26 +0900

    update solution testcaes

--- cote/src/exam/Solution0901_2.java
+++ cote/src/exam/Solution0901_2.java
@@ -5,9 +5,37 @@ public class Solution0901_2 {
     public static void main(String[] args) {
         Solution0901_2 solution = new Solution0901_2();
 
-        String str = "abababaaaaaaaa";
-        int result = solution.solution(str);
-        System.out.println(str + " : result = " + result);
+        String[][] tcArr = {
+            {"aabbaccc", "7"},
+            {"ababcdcdababcdcd", "9"},
+            {"abcabcdede", "8"},
+            {"abcabcabcabcdededededede", "14"},
+            {"xababcdcdababcdcd", "17"}
+        };
+
+        double cnt = 0;
+        double correct = 0;
+        double incorrect = 0;
+
+        for(String[] tc : tcArr){
+            cnt++;
+
+            String input  = tc[0];
+            int    output = Integer.parseInt(tc[1]);
+            
+            int    result       = solution.solution(input);
+            String resultString = "input : " + input + ", output : " + result + ", result : " + output;
+            
+            if(result == output){
+                correct++;
+            }else{
+                incorrect++;
+                System.out.print(resultString);
+                System.out.println("\tincorrect");
+            }
+        }
+
+        System.out.printf("\n수행 : %d, 정답 : %d/%d(%.2f%%)", (int)cnt, (int)correct,(int)cnt, correct/cnt*100);
     }
 
     public int solution(String s){
