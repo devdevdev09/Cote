89e1ab6f8c41a6f72cd1fa99c3d22f430572b2a1
dev09 <devdevdevdevdev09@gmail.com>
2020-09-18 15:04:15 +0900

    regex test

--- cote/src/exam/s0917/Solution.java
+++ cote/src/exam/s0917/Solution.java
@@ -1,48 +1,44 @@
 package exam.s0917;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
-        String s = "cdcd";
+        String[] s = {"cdcd","baabaa"};
+        int[] a = {0,1};
         
         Solution sol = new Solution();
-        int n = sol.solution(s);
-        System.out.println("n : " + n);
+        int idx = 0;
+
+        for(String str : s){
+            int result = 0;
+            result = sol.solution(str);
+            
+            if(result == a[idx++]){
+                System.out.println(idx + " : success");
+            }else{
+                System.out.println(idx + " : fail");
+            }
+        }
     }
 
-    public int solution(String s)
-    {
-        int answer = 1;
-        
+    public int solution(String s) {
+        String[] sArr = Arrays.stream(s.split("")).distinct().toArray(String[]::new);
+
         String tmp = s;
-        while(answer > 0){
-            tmp = getString(tmp);
-            if(tmp.equals("")){
-                return 1;
-            }
+        while(!tmp.equals("")) {
+            String pre = "";
 
-            if(s.equals(tmp)){
-                return 0;
+            for(int i = 0; i < sArr.length; i++) {
+                pre = tmp;
+                tmp = pre.replaceAll("[" + sArr[i] + "]{2}", "");
+                if(tmp.length() == 0) return 1;
             }
-
-            answer = tmp.length();
+            if(pre.equals(tmp)) return 0;
+            if(tmp.equals(s)) return 0;
         }
 
-        if(s.length() == 0){
-            return 1;
-        }
-
-        return 0;
+        if(tmp.length() == 0) return 1;
+        else return 0;
     }
-
-    public String getString(String s){
-
-        for(int i = 0 ; i < s.toCharArray().length-1; i++){
-            if(s.charAt(i) == s.charAt(i+1)){
-                return s.substring(0, i) + s.substring(i+2);
-            }
-        }
-
-        return s;
-    }
-
 }
