d9492209be9b7e69de4f5bf9610fd86b6d0a715e
dev09 <devdevdevdevdev09@gmail.com>
2020-09-23 17:11:36 +0900

    실패 폴더 구분

---
+++ cote/src/exam/s0917_X/Solution.java
@@ -0,0 +1,60 @@
+package exam.s0917_X;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] s = {"cdcd","baabaa"};
+        int[] a = {0,1};
+        
+        Solution sol = new Solution();
+        int idx = 0;
+
+        for(String str : s){
+            int result = 0;
+            result = sol.solution(str);
+            
+            if(result == a[idx++]){
+                System.out.println(idx + " : success");
+            }else{
+                System.out.println(idx + " : fail");
+            }
+        }
+    }
+
+    public int solution(String s)
+    {
+        int answer = 1;
+
+        String tmp = s;
+        while(answer > 0){
+            tmp = getString(tmp);
+            if(tmp.equals("")){
+                return 1;
+            }
+
+            if(s.equals(tmp)){
+                return 0;
+            }
+
+            answer = tmp.length();
+        }
+
+        if(s.length() == 0){
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public String getString(String s){
+
+        for(int i = 0 ; i < s.toCharArray().length-1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                return s.substring(0, i) + s.substring(i+2);
+            }
+        }
+
+        return s;
+    }
+}
