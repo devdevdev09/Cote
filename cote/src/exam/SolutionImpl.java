a8ab141d167735100befc6114cf982958b9c7600
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-02-18 23:43:37 +0900

    real solv

--- cote/src/exam/SolutionImpl.java
+++ cote/src/exam/SolutionImpl.java
@@ -3,7 +3,10 @@ package exam;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class SolutionImpl implements Solution{
@@ -34,69 +37,30 @@ class Solv {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
-        List<Integer> list = convert(scoville);
-        list = sort(list);
-        
-        while(true){
-            int[] arr1 = getMinSco2(list);
-            list = getMinSco3(list);
+        Queue list = new PriorityQueue<>();
 
+        for(int n : scoville){
+            list.add(n);
+        }
+        
+        while(!list.isEmpty()){
             ++answer;
-            int num = sumScovile(arr1[0], arr1[1]);
-            list = concat(list, num);      
-                        
-            if(list.size() == 1 && num <= K)
-                return -1;
-            
-            boolean result = check(list, K);
-            if(!result)
+
+            int num1 = (int)list.poll();
+            int num2 = (int)list.poll();
+
+            int sum = num1 + (num2*2);
+            list.add(sum);
+
+            if((int)list.peek() >= K){
                 return answer;
+            }
+
+            if(list.size() == 1 && (int)list.peek() < K){
+                return -1;
+            }
         }
+        return answer;
     }
     
-    public List<Integer> convert(int[] arr){
-        return  Arrays.stream(arr).boxed().collect(Collectors.toList());
-    }
-    
-    public List<Integer> sort(List<Integer> list){
-        Collections.sort(list);
-        return list;
-    }
-    
-    public boolean check(List<Integer> list, int k){
-        List<Integer> result = new ArrayList<>(); 
-        result.addAll(list);
-        result.removeIf(i->(i>=k));
-        
-        if(result.size() == 0){
-            return false;
-        }else{
-            return true;
-        }
-    }
-    
-    
-    public int sumScovile(int sco1, int sco2) {
-        return sco1 + (sco2*2);
-    }
-    
-    public int[] getMinSco2(List<Integer> scoville) {
-        int[] arr = new int[2];
-        arr[0] = scoville.get(0);
-        arr[1] = scoville.get(1);
-        return arr;
-    }
-    
-    public List<Integer> getMinSco3(List<Integer> scoville) {
-        scoville.remove(0);
-        scoville.remove(0);
-        return scoville;
-        
-    }
-    
-    public List<Integer> concat(List<Integer> list, int num){
-        list.add(num);
-        Collections.sort(list);
-        return list;
-    }
 }
